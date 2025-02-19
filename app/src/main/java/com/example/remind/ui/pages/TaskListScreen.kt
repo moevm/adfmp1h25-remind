b54cb8155edda011715fc0099e35b7d4693bb617
SyrtcevaDaria <syrtceva566333@gmail.com>
2025-02-19 23:12:01 +0300

    feat: add task list

---
+++ app/src/main/java/com/example/remind/ui/pages/TaskListScreen.kt
@@ -0,0 +1,31 @@
+package com.example.remind.ui.pages
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TaskListScreen(tasks: List<String>) {
+    val isEmpty = tasks.isEmpty()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (isEmpty) {
+            EmptyScreen(onAddClick = {
+
+            })
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(tasks) { task ->
+                    Text(
+                        text = task,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
